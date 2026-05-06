6f0d7927cb88053d24f54c111c21dd8950063f14
Albert G. <252793@edu.p.lodz.pl>
2026-05-06 11:32:17 +0200

    Changed Layout component to use TailwindCSS instead of CSS.

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,27 +2,27 @@ import React from "react";
 
 import { Link } from "@lsg/components";
 
-import "./Layout.css";
-
 interface LayoutProps {
   children: React.ReactNode;
 }
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
-    <div className="layout">
-      <header className="layout_header">
+    <div className="flex flex-col min-h-screen">
+      <header className="flex items-center justify-between bg-primary text-secondary px-8 py-4">
         <Link href="/">LOGO</Link>
 
-        <nav className="layout_nav">
+        <nav className="flex items-center gap-4">
           <Link href="/">Home</Link>
           <Link href="/">Home</Link>
         </nav>
       </header>
 
-      <main className="layout_content">{children}</main>
+      <main className="flex-1 flex flex-col items-center justify-center p-8">
+        {children}
+      </main>
 
-      <footer className="layout_footer">
+      <footer className="bg-primary text-secondary text-center px-8 py-4">
         <p>Projekt Innowacja 2026</p>
       </footer>
     </div>
