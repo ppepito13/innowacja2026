de25b57bf9283ad523469c3d9047599f7faa9cef
Natalia Balicka <nbalicka35@gmail.com>
2026-05-15 15:43:45 +0200

    types replaced models

--- src/models/index.ts
+++
@@ -1,8 +0,0 @@
-export { ParseEvent } from "./Event";
-export type { IEvent } from "./Event";
-
-export { Registration } from "./Registration";
-export type { IRegistration, RegistrationStatus } from "./Registration";
-
-export { AppUser } from "./User";
-export type { IAppUser, UserRole } from "./User";
