fb564553482c9d1824cbc7afc13c9eaf94fd53c6
Administrator <gitlab_admin_16c3f1@example.com>
2026-05-27 20:58:47 +0200

    server

---
+++ server/config.js
@@ -0,0 +1,15 @@
+const config = {
+  "appId": "innowacje2026",
+  "masterKey": "fa114fa1-1ddc-49f1-8034-a8ab414fa558",
+  "appName": "Innowacje 2026",
+  "databaseURI": "mongodb://localhost:27017/innowacje2026",
+  "serverURL": "http://127.0.0.1:1337/parse",
+  "publicServerURL": "https://192.168.66.24:1337/parse",
+  "port": 1337,
+  "liveQuery": {
+    "classNames": []
+  },
+  "masterKeyIps": ['0.0.0.0/0', '::/0']
+}
+
+export default config;
