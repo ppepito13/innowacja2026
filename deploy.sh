addc2f6d98ad356746bda9432d31fe4e33e338d5
Administrator <gitlab_admin_16c3f1@example.com>
2026-08-16 23:26:00 +0200

    deploy script update

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,8 @@
 npm run build
 zip -r build.zip build
+
 scp -r build.zip administrator@192.168.66.24:~/
+# scp server/cloud/main.js administrator@192.168.66.24:~/
+# scp server/config.js administrator@192.168.66.24:~/
+
+ssh administrator@192.168.66.24 '/home/administrator/deploy.sh'
